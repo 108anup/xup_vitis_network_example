bb7ea2c0c0b35651d2f8fcce6a6bbd0ecdbd2ec0
Anup Agarwal <anupa@andrew.cmu.edu>
2021-02-10 16:44:06 -0500

    Make free running kernel

--- Sketch/src/cm_sketch.cpp
+++ Sketch/src/cm_sketch.cpp
@@ -8,6 +8,7 @@
 
 typedef ap_axiu<DWIDTH, 1, 1, TDWIDTH> pkt;
 unsigned int cm_sketch_local[cm_rows][cm_col_count];
+#pragma HLS ARRAY_PARTITION variable = cm_sketch_local complete dim = 1
 
 unsigned int MurmurHash2(unsigned int key, int len, unsigned int seed)
 {
@@ -30,8 +31,11 @@ extern "C" {
   // sits between benchmark switch and network layer
   void update_sketch(hls::stream<pkt> &dataIn,
                      hls::stream<pkt> &dataOut) {
+#pragma HLS DATAFLOW
 #pragma HLS INTERFACE axis port=dataIn
 #pragma HLS INTERFACE axis port=dataOut
+#pragma HLS INTERFACE ap_ctrl_none port=return
+DO_PRAGMA(HLS ALLOCATION function instances=MurmurHash2 limit=HASH_UNITS)
 
     pkt curr;
     unsigned int key;
@@ -39,6 +43,8 @@ extern "C" {
     // Only works with 64 B packets right now
     if(!dataIn.empty()){
       dataIn.read(curr);
+
+      // Short
       key = curr.data(31, 0);
       // (39, 0) corresponds to packet counter (using as uniform random key currently)
 
@@ -48,13 +54,13 @@ extern "C" {
         unsigned index = hash % cm_col_count;
         cm_sketch_local[row][index]++;
       }
+
       dataOut.write(curr);
     }
   }
 
   void read_sketch(unsigned int* sketch_buf) {
 #pragma HLS INTERFACE m_axi port=sketch_buf bundle=gmem offset=slave
-
   write_cm_sketch: for(unsigned iter = 0, row = 0, col = 0;
                        iter < cm_col_count * cm_rows; iter++, col++) {
 #pragma HLS PIPELINE II=1
