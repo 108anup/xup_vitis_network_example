0969db808e5c0c29c2755f94755609e3d34c538c
Anup Agarwal <anupa@andrew.cmu.edu>
2021-02-16 19:10:36 -0500

    Add tools

---
+++ tools/compile_manifest.py
@@ -0,0 +1,63 @@
+import yaml
+import sys
+import subprocess
+import os
+from pathlib import Path
+from multiprocessing.pool import ThreadPool
+
+PROJECT_DIR = "../"
+STATUS_DIR = "build_status"
+PARALLELISM = 2
+
+
+def compile_manifest_util(manifest):
+    # Currently only implementing one sketch at a time
+    sketch = manifest['sketches'][0]
+    rows = sketch['rows']
+    logcols = sketch['logcols']
+    hash_units = sketch['hash_units']
+    # https://www.saltycrane.com/blog/2011/04/how-use-bash-shell-python-subprocess-instead-binsh/
+    cmd = ["make", "all", "DEVICE=xilinx_u280_xdma_201920_3",
+           "INTERFACE=1", "DESIGN=benchmark", "SKETCH=1",
+           "CM_ROWS={}".format(rows),
+           "CM_COLS={}".format(logcols),
+           "HASH_UNITS={}".format(hash_units)]
+    cmd_string = " ".join(cmd)
+    
+    # Just for testing if working properly
+    # cmd_string = "echo \"{}\"; sleep 10".format(cmd_string)
+
+    print("Running: {}".format(cmd))
+    fout = open(os.path.join(
+        STATUS_DIR,
+        "{}-{}-{}.stdout".format(hash_units, rows, logcols)),
+                "w")
+    ferr = open(os.path.join(
+        STATUS_DIR,
+        "{}-{}-{}.stderr".format(hash_units, rows, logcols)),
+                "w")
+    # https://stackoverflow.com/questions/4856583/how-do-i-pipe-a-subprocess-call-to-a-text-file
+    p = subprocess.Popen(cmd_string, shell=True, stdout=fout, stderr=ferr)
+    p.wait()
+    fout.close()
+    ferr.close()
+
+
+manifests_file = sys.argv[1]
+with open(manifests_file) as f:
+    manifests = yaml.safe_load(f)
+
+os.chdir(PROJECT_DIR)
+# https://stackoverflow.com/questions/273192/how-can-i-safely-create-a-nested-directory
+Path(STATUS_DIR).mkdir(parents=True, exist_ok=True)
+
+# https://stackoverflow.com/questions/26774781/python-multiple-subprocess-with-a-pool-queue-recover-output-as-soon-as-one-finis
+tp = ThreadPool(PARALLELISM)
+
+print("Total manifests:", len(manifests))
+for manifest in manifests:
+    tp.apply_async(compile_manifest_util, (manifest,))
+
+# https://stackoverflow.com/questions/35708371/purpose-of-pool-join-pool-close-in-multiprocessing
+tp.close()
+tp.join()
---
+++ tools/generate_manifest.py
@@ -0,0 +1,53 @@
+import yaml
+import sys
+# A proper manifest would have flow filters instead of thr
+
+hash_bench = [
+    {
+        'sketches': [
+            {
+                'rows': r,
+                'logcols': 6,
+                'thr': 1,
+                'frac': 1,
+                'hash_units': 4
+            }
+        ],
+        'total_thr': 1
+    }
+    for r in range(1, 5)
+]
+
+# mem_bench = [
+#     {
+#         'sketches': [
+#             {
+#                 'rows': r,
+#                 'cols': 2**logc,
+#                 'thr': 1,
+#                 'frac': 1
+#             }
+#         ],
+#         'total_thr': 1
+#     }
+#     for r in [10, 12]
+#     for logc in range(23)
+# ]
+
+# ground_truth = [
+#     {
+#         'sketches': [
+#             {
+#                 'rows': r,
+#                 'cols': 4**logc,
+#                 'thr': 1,
+#                 'frac': 1
+#             }
+#         ],
+#         'total_thr': 1
+#     }
+#     for r in range(2, 10, 2)
+#     for logc in range(2, 12)
+# ]
+
+yaml.dump(hash_bench, sys.stdout)
