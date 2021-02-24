5e9aa5576ee6514e7d253970590fb80125155f24
Anup Agarwal <anupa@andrew.cmu.edu>
2021-02-24 14:41:10 -0500

    Add amdahls manifest

--- tools/generate_manifest.py
+++ tools/generate_manifest.py
@@ -18,21 +18,39 @@ hash_bench = [
     for r in range(1, 5)
 ]
 
-# mem_bench = [
-#     {
-#         'sketches': [
-#             {
-#                 'rows': r,
-#                 'cols': 2**logc,
-#                 'thr': 1,
-#                 'frac': 1
-#             }
-#         ],
-#         'total_thr': 1
-#     }
-#     for r in [10, 12]
-#     for logc in range(23)
-# ]
+mem_bench = [
+    {
+        'sketches': [
+            {
+                'rows': r,
+                'logcols': logc,
+                'thr': 1,
+                'frac': 1,
+                'hash_units': r
+            }
+        ],
+        'total_thr': 1
+    }
+    for r in [8, 10]
+    for logc in [6, 8, 10]
+]
+
+amdahls_hash = [
+    {
+        'sketches': [
+            {
+                'rows': 12,
+                'logcols': 6,
+                'thr': 1,
+                'frac': 1,
+                'hash_units': h
+            }
+        ],
+        'total_thr': 1
+    }
+    for h in range(1, 7)
+]
+
 
 # ground_truth = [
 #     {
@@ -50,4 +68,4 @@ hash_bench = [
 #     for logc in range(2, 12)
 # ]
 
-yaml.dump(hash_bench, sys.stdout)
+yaml.dump(amdahls_hash, sys.stdout)
